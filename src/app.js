5350a9226b4fcdb0214f1c27fba646fb819a762f
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-12-16 14:10:28 +0000

    API getter added in NodeJS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
 const express =  require('express');
 const app = express();
 const request = require('request');
+
+
+
+const getQuiz = (catNum = "", callback)=>{
+// const getQuiz = ((callback)=>{
+
+    const quizURL = `https://opentdb.com/api.php?amount=10&type=multiple`;
+
+    if (typeof catNum == "number"){
+        let catagorySetter = `&category=${catNum}`
+        const quizURL = `https://opentdb.com/api.php?amount=10&type=multiple${catagorySetter}`;
+    } 
+
+    request({url: quizURL, json:true}, async (err, response )=> {
+        
+        if(err){
+
+            console.log("ERROR: Cannot connect to API");
+            
+        }else if(response == undefined){
+
+            callback({
+                error: "Cannot find this catagory"
+            });
+        }
+        else{
+            
+            callback(response);
+       }
+      
+    })
+}
+
+getQuiz(2, (response)=>{
+    console.log(response);
+})
